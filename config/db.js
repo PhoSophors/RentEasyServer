c92688a89822c758ba072db88f43fdb25a7174e2
PhoSophors <phosophors.097@gmail.com>
2024-08-29 14:46:18 +0700

    update db

--- config/db.js
+++ config/db.js
@@ -1,25 +1,18 @@
+// db.js
+
 const mongoose = require('mongoose');
-require('dotenv').config({ path: '../.env' });
 
-const mongoURI = process.env.MONGO_URI;
-
-module.exports = async () => {
-    try {
-        console.log('DB_URL:', mongoURI);
-        await mongoose.connect(mongoURI, {
-            connectTimeoutMS: 30000,
-            serverSelectionTimeoutMS: 50000,
-            socketTimeoutMS: 45000,
-        });
-        console.log("CONNECTED TO DATABASE SUCCESSFULLY");
-    } catch (error) {
-        console.error('COULD NOT CONNECT TO DATABASE:', error.message);
-        process.exit(1);
-    }
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); 
+  }
 };
 
-if (require.main === module) {
-    (async () => {
-        await module.exports();
-    })();
-}
+module.exports = connectDB;
