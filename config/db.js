53aaa80c3d2fad476d03c75a784104dc45cf5100
PhoSophors <phosophors.097@gmail.com>
2024-08-29 16:06:19 +0700

    db

--- config/db.js
+++ config/db.js
@@ -3,12 +3,22 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    console.error('MONGODB_URI environment variable is not set');
+    process.exit(1);
+  }
+
+  console.log('Attempting to connect to MongoDB...');
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected...');
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected successfully');
   } catch (err) {
-    console.error(err.message);
-    process.exit(1); 
+    console.error('Error connecting to MongoDB:', err.message);
+    process.exit(1);
   }
 };
 
