5d59189f3be35416aadf709434c7ec00c57605be
PhoSophors <phosophors.097@gmail.com>
2024-08-29 12:58:59 +0700

    update database

--- config/db.js
+++ config/db.js
@@ -1,22 +1,34 @@
-// config/db.js
+// // config/db.js
+
+// const mongoose = require('mongoose');
+// require('dotenv').config();
+
+// const mongoURI = process.env.MONGO_URI;
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(mongoURI, {
+//       connectTimeoutMS: 30000, 
+//       serverSelectionTimeoutMS: 50000,
+//       socketTimeoutMS: 45000,
+//     });
+//     console.log('MongoDB connected successfully');
+//   } catch (err) {
+//     console.error('MongoDB connection error:', err);
+//     process.exit(1); // Exit process with failure
+//   }
+// };
+
+// module.exports = connectDB;
 
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const mongoURI = process.env.MONGO_URI;
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(mongoURI, {
-      connectTimeoutMS: 30000, 
-      serverSelectionTimeoutMS: 50000,
-      socketTimeoutMS: 45000,
-    });
-    console.log('MongoDB connected successfully');
-  } catch (err) {
-    console.error('MongoDB connection error:', err);
-    process.exit(1); // Exit process with failure
-  }
+module.exports = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {});
+        console.log("CONNECTED TO DATABASE SUCCESSFULLY");
+    } catch (error) {
+        console.error('COULD NOT CONNECT TO DATABASE:', error.message);
+    }
 };
-
-module.exports = connectDB;
